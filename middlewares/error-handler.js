8d3eadfb9c232c0248786bdaa3cb267ac906b4d1
kaunghtet466 <kh685132@gmail.com>
2022-08-16 11:04:53 +0630

    changes

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -3,6 +3,12 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || 'internal server error',
     statusCode: err.statusCode || 500
   }
+
+  if (err.name === 'CastError') {
+    const { value: { _id: blogID } } = err
+    customError.msg = `cannot find the blog with id : ${blogID}`
+    customError.statusCode = 401
+  }
   //return res.status(customError.statusCode).json({ err })
   return res.status(customError.statusCode).json({ msg: customError.msg })
 }
